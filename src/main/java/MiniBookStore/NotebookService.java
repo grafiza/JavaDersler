b23392a90c6c2bfd4410a22a2deefe05ff2b1688
Zafer Kanbur <zaferkanbur@hotmail.com>
2023-09-26 14:24:25 +0300

    books2

--- src/main/java/MiniBookStore/NotebookService.java
+++ src/main/java/MiniBookStore/NotebookService.java
@@ -82,21 +82,135 @@ public class NotebookService implements ProductService{
 
     @Override
     public void addProduct() {
+        System.out.println("Ürün Kodu : ");
+        String code = inp.nextLine();
 
+        boolean isExists = false;
+        for (Notebook noteBook : this.noteBookList) {
+            if (noteBook.getCode().equals(code)) {
+                System.out.println("Bu defter sistemde kayıtlı, lütfen ürün güncelleme yapınız.");//stok güncelleme:ÖDEV
+                isExists = true;
+                break;
+            }
+        }
+        if (!isExists) {
+            System.out.println("Defter Adı :");
+            String name = inp.nextLine();
+            System.out.println("Defter Markası :");
+            String brand = inp.nextLine();
+            System.out.println("Fiyatı:");
+            String price = inp.nextLine();
+            System.out.println("Sayfa Sayısı :");
+            String sheet = inp.nextLine();
+            System.out.println("Stok : ");
+            int stock = inp.nextInt();
+            inp.nextLine();
+
+            Notebook newNotebook=new Notebook(name,price,stock,brand,sheet,code);
+            this.noteBookList.add(newNotebook);
+        }
+        listProducts();
     }
 
     @Override
     public void deleteProduct() {
-
+        boolean isFound = false;
+        System.out.println("Defter id : ");
+        int id = inp.nextInt();
+        for (Notebook noteBook : this.noteBookList) {
+            if (noteBook.getId() == id) {
+                this.noteBookList.remove(noteBook);
+                System.out.println("Ürün silindi.");
+                isFound = true;
+                break;
+            } else {
+                isFound = false;
+            }
+        }
+        if (!isFound) {
+            System.out.println("Ürün bulunamadı!");
+        }
     }
 
     @Override
     public void filterProducts(String filter) {
+        int counter = 0;
+        for (Notebook notebook : this.noteBookList) {
+            if (notebook.getName().equalsIgnoreCase(filter)) {
 
+                System.out.printf("%-3s | %-20s | %-15s | %-15s | %-15s | %-15s | %-10s | \n"
+                        ,notebook.getId(),notebook.getName(),notebook.getBrand(),notebook.getPrice(),notebook.getSheet(),notebook.getStock(),notebook.getCode()
+                );
+                counter++;
+            } else if (notebook.getBrand().equalsIgnoreCase(filter)) {
+                System.out.printf("%-3s | %-20s | %-15s | %-15s | %-15s | %-15s | %-10s | \n"
+                        ,notebook.getId(),notebook.getName(),notebook.getBrand(),notebook.getPrice(),notebook.getSheet(),notebook.getStock(),notebook.getCode()
+                );
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            System.out.println("Eşleşen ürün bulunamadı!");
+        }
     }
 
     @Override
     public void updateProducts() {
+        int id;
+        System.out.println("1. Adet Güncelleme");
+        System.out.println("2. Fiyat Güncelleme");
+        int secim = inp.nextInt();
+        inp.nextLine();
+        switch (secim) {
+            case 1:
+                System.out.println("Defter id : ");
+                id = inp.nextInt();
+                inp.nextLine();
+                stokGuncelle(id);
+                break;
 
+            case 2:
+                System.out.println("Defter id : ");
+                id = inp.nextInt();
+                inp.nextLine();
+                fiyatGuncelle(id);
+            default:
+                System.out.println("Hatalı Giriş");
+        }
     }
+
+    private void fiyatGuncelle(int id) {
+        boolean control = false;
+        for (Notebook notebook : this.noteBookList) {
+            if (notebook.getId() == id) {
+                System.out.println("Yeni Fiyat:");
+                String yeniFiyat = inp.nextLine();
+                notebook.setPrice(yeniFiyat);
+                System.out.println("Fiyat bilgisi güncellendi.");
+                control = true;
+                break;
+            }
+        }
+        if (!control)
+            System.out.println("Ürün Bulunamadı.");
+    }
+
+    private void stokGuncelle(int id) {
+        boolean control = false;
+        for (Notebook notebook : this.noteBookList) {
+            if (notebook.getId() == id) {
+                System.out.println("Yeni Stok:");
+                int yeniStok = inp.nextInt();
+                inp.nextLine();
+                notebook.setStock(yeniStok);
+                System.out.println("Stok bilgisi güncellendi.");
+                control = true;
+                break;
+            }
+        }
+        if (!control)
+            System.out.println("Ürün Bulunamadı.");
+    }
+
+
 }
